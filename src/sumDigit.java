b2f4c4ed86174b1b064db8743b304d9fc2adc884
Aditi Priya <aditiabhaykumar@gmail.com>
2019-10-15 17:07:04 -0400

    Adding all

--- src/sumDigit.java
+++ src/sumDigit.java
@@ -1,26 +1,28 @@
 public class sumDigit {
     public static void main(String[] args) {
-        sumDigits(678);
+        System.out.println(sumDigits(555555));
     }
 
     public static int sumDigits(int number) {
-        int sum = 0;
-       /* if (number < 10) {
-            return -1;
-        }
-            while (number != 0) {
-                sum = sum + number % 10;
-                number = number % 10;
-                System.out.println("number" + sum);
-                break;
-            }
-        }
-//        System.out.println(sum);
-//        return 0;
-        else if (number <= 0 && number <= 9) {
-            return -1;
-        }*/
-        return 0;
 
+        int sum = 0;
+        int remainder = 0;
+        int quotient = 0;
+        while (number > 10) {
+            remainder = number % 10;
+            quotient = number / 10;
+            if (quotient < 10) {
+                sum += quotient;
+                number=0;
+            } else {
+                number = quotient;
+            }
+            if (remainder > 0) {
+                sum += remainder;
+            }
+           // System.out.println(number);
+        }
+
+        return sum;
     }
 }
