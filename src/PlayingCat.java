0196c66f6e052a018c0db50887a7835d8831a0d9
Aditi Priya <aditiabhaykumar@gmail.com>
2019-10-15 17:07:52 -0400

    Adding all

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,10 +1,21 @@
 public class PlayingCat {
     public static void main(String[] args) {
-        isCatPlaying(true,34);
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
     }
-    public static void isCatPlaying(boolean Summer,int temperature)
-    {
 
-
+    public static boolean isCatPlaying(boolean Summer, int temperature) {
+        boolean isCatplaying = false;
+        if (Summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                isCatplaying = true;
+            }
+        } else if (!Summer) {
+            if (temperature >= 25 && temperature <= 35) {
+                isCatplaying = true;
+            }
+        }
+        return isCatplaying;
     }
 }
