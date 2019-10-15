f7e4f50a1ca37f725c1dc48a27487801fad718bc
Aditi Priya <aditiabhaykumar@gmail.com>
2019-10-15 17:07:40 -0400

    Adding all

--- src/ReadingInputUser.java
+++ src/ReadingInputUser.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 public class ReadingInputUser {
     public static void main(String[] args) {
 
-
+        int sum = 0;
+        int count = 0;
         System.out.println("Enter the number");
-        Scanner scanner=new Scanner(System.in);
-        int number=scanner.nextInt();
-        while(number<=10);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
+        {
+            sum += number;
+            count++;
+        }
+        if (count == 10) {
+            System.out.println("Invalid");
+        }
+
 
     }
 
