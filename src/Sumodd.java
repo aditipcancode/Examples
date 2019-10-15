2b7874a49c4886074a609089fc22164f0a00e0d4
Aditi Priya <aditiabhaykumar@gmail.com>
2019-10-15 17:07:19 -0400

    Adding all

--- src/Sumodd.java
+++ src/Sumodd.java
@@ -1,26 +1,46 @@
 public class Sumodd {
     public static void main(String[] args) {
-        //boolean true;
-        // boolean false;
-        isOdd(8);
+
+        System.out.println(isOdd(8));
+        System.out.println(isOdd(-8));
+        System.out.println(isOdd(7));
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(13, 13));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
     }
 
     public static boolean isOdd(int number) {
-        if (number > 0) {
-            if (number % 2 == 0) {
-                System.out.println(number + "is even number");
-                return false;
-            } else {
-                System.out.println(number + "is odd number");
-                return true;
-            }
-        } else {
-            return true;
-        }
-    }
-}
+        boolean isOdd = false;
 
-//    private static int sumOdd(int start,int end)
-//    {
-//
-//    }
+        if (number > 0) {
+            if (number % 2 != 0) {
+                isOdd = true;
+            }
+        }
+        return isOdd;
+    }
+
+
+    private static int sumOdd(int start, int end) {
+        int sumOdd = -1;
+        int sum = 0;
+        if (start > 0 && end >= start) {
+            for (int i = start; i <= end; i++) {
+
+                if (isOdd(i))
+                    sum += i;
+
+
+            }
+            return sum;
+        }
+        {
+            return -1;
+        }
+
+    }
+
+}
